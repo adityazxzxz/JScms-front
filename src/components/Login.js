77c458c92d11fd40cc1588e192c0d1ecf9f43fbe
Aditya <adityapratamax11@gmail.com>
2020-09-26 15:52:11 +0700

    desain login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -37,14 +37,46 @@ const Login = (props) => {
 
 
 
+    // return (
+    //     <div>
+    //         <h1>Login</h1>
+    //         <form onSubmit={onSubmit}>
+    //             <input type='text' name='email' onChange={(e) => setEmail(e.target.value)} placeholder='Email' />
+    //             <input type='password' name='password' onChange={(e) => setPassword(e.target.value)} placeholder='Email' />
+    //             <button>Login</button>
+    //         </form>
+    //     </div>
+    // )
+
     return (
-        <div>
-            <h1>Login</h1>
-            <form onSubmit={onSubmit}>
-                <input type='text' name='email' onChange={(e) => setEmail(e.target.value)} placeholder='Email' />
-                <input type='password' name='password' onChange={(e) => setPassword(e.target.value)} placeholder='Email' />
-                <button>Login</button>
-            </form>
+        <div id="login">
+        <h3 className="text-center text-white pt-5">Login form</h3>
+        <div className="container">
+            <div id="login-row" className="row justify-content-center align-items-center">
+                <div id="login-column" className="col-md-6">
+                    <div id="login-box" className="col-md-12">
+                        <form id="login-form" className="form" onSubmit={onSubmit}>
+                            <h3 className="text-center text-info">Login</h3>
+                            <div className="form-group">
+                                <label htmlFor="username" className="text-info">Username:</label><br />
+                                <input type="text" name="username" id="username" className="form-control" onChange={(e) => setEmail(e.target.value)} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password" className="text-info">Password:</label><br />
+                                <input type="password" name="password" id="password" className="form-control" onChange={(e) => setPassword(e.target.value)} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="remember-me" className="text-info"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox" /></span></label><br />
+                                <button className="btn btn-info btn-md">Submit</button>
+                            </div>
+                            <div id="register-link" className="text-right">
+                                <a href="#" className="text-info">Register here</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
